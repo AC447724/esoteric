bd5ebd64cdefb8196b86acd729c474f231c6004d
VXP..cvxpk <124467084+AC447724@users.noreply.github.com>
2026-04-24 11:16:16 -0400

    Update star-of-david-crosshair.lua

--- a/api/star-of-david-crosshair.lua
+++ a/api/star-of-david-crosshair.lua
@@ -1,16 +1,10 @@
-local cfg = _G.StarConfig or {
-	Mode = "Mouse",
-	CustomPos = Vector2.new(400, 400),
-	Size = 50,
-	LineWidth = 2,
-	ColorHex = "#0038B8"
-}
+local cfg = _G.StarConfig or {Mode = "Mouse", Pos = Vector2.new(400, 400), Size = 50, Width = 2, Hex = "#0038B8"}
 
 local rs, uis = game:GetService("RunService"), game:GetService("UserInputService")
 local lp, cam = game.Players.LocalPlayer, workspace.CurrentCamera
 local v2, ud2 = Vector2.new, UDim2.new
 
-local r, g, b = cfg.ColorHex:gsub("#",""):match("(..)(..)(..)")
+local r, g, b = cfg.Hex:gsub("#",""):match("(..)(..)(..)")
 local c = Color3.fromRGB(tonumber(r, 16), tonumber(g, 16), tonumber(b, 16))
 
 local g = Instance.new("ScreenGui", lp:WaitForChild("PlayerGui"))
@@ -31,13 +25,13 @@ local v = {
 }
 
 local function draw(l, p1, p2)
-	l.Size = ud2(0, (p1 - p2).Magnitude, 0, cfg.LineWidth)
+	l.Size = ud2(0, (p1 - p2).Magnitude, 0, cfg.Width)
 	l.Position = ud2(0, (p1.X + p2.X) / 2, 0, (p1.Y + p2.Y) / 2)
 	l.Rotation = math.deg(math.atan2(p2.Y - p1.Y, p2.X - p1.X))
 end
 
 rs.RenderStepped:Connect(function()
-	local gp = cfg.CustomPos
+	local gp = cfg.Pos
 	
 	if cfg.Mode == "Mouse" then
 		gp = uis:GetMouseLocation()
