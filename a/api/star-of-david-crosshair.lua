7ccd49d92991b691e5a674f0403026e126c0b051
VXP..cvxpk <124467084+AC447724@users.noreply.github.com>
2026-04-24 11:21:26 -0400

    Update star-of-david-crosshair.lua

--- a/api/star-of-david-crosshair.lua
+++ a/api/star-of-david-crosshair.lua
@@ -1,12 +1,11 @@
-local cfg = _G.StarConfig or {Mode = "Mouse", Pos = Vector2.new(400, 400), Size = 50, Width = 2, Hex = "#0038B8"}
+-- left open src bc this shit was "promised" to israel 2000 years ago or whatever the fuck 
 
+local cfg = _G.StarConfig or {Mode = "Mouse", Pos = Vector2.new(400, 400), Size = 50, Width = 2, Hex = "#0038B8"}
 local rs, uis = game:GetService("RunService"), game:GetService("UserInputService")
 local lp, cam = game.Players.LocalPlayer, workspace.CurrentCamera
 local v2, ud2 = Vector2.new, UDim2.new
-
 local r, g, b = cfg.Hex:gsub("#",""):match("(..)(..)(..)")
 local c = Color3.fromRGB(tonumber(r, 16), tonumber(g, 16), tonumber(b, 16))
-
 local g = Instance.new("ScreenGui", lp:WaitForChild("PlayerGui"))
 g.Name, g.IgnoreGuiInset = "StarGui", true
 
@@ -23,7 +22,6 @@ local v = {
 	v2(0, -rad * 2), v2(-cfg.Size / 2, rad), v2(cfg.Size / 2, rad),
 	v2(0, rad * 2), v2(-cfg.Size / 2, -rad), v2(cfg.Size / 2, -rad)
 }
-
 local function draw(l, p1, p2)
 	l.Size = ud2(0, (p1 - p2).Magnitude, 0, cfg.Width)
 	l.Position = ud2(0, (p1.X + p2.X) / 2, 0, (p1.Y + p2.Y) / 2)
@@ -32,13 +30,12 @@ end
 
 rs.RenderStepped:Connect(function()
 	local gp = cfg.Pos
-	
 	if cfg.Mode == "Mouse" then
 		gp = uis:GetMouseLocation()
 	elseif cfg.Mode == "Center" then
 		gp = cam.ViewportSize / 2
 	end
-
+		
 	draw(lines[1], gp + v[1], gp + v[2])
 	draw(lines[2], gp + v[2], gp + v[3])
 	draw(lines[3], gp + v[3], gp + v[1])
